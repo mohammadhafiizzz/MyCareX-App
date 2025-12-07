691dfcde5dd2207d1490894b882355f4f5808e3f
Hafiz <mohammadhafiizzzz@gmail.com>
2025-12-07 13:55:35 +0800

    Add controllers for hospitalisation and surgery modules

---
+++ app/Http/Controllers/Modules/Hospitalisation/DeleteHospitalisationController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Modules\Hospitalisation;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DeleteHospitalisationController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Modules/Surgery/UpdateSurgeryController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Modules\Surgery;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UpdateSurgeryController extends Controller
+{
+    //
+}
