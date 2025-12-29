6410d8769ba4e0e38a22f3f6a20e0704bea49fc6
Hafiz <mohammadhafiizzzz@gmail.com>
2025-12-29 19:20:11 +0800

    feat: add fast-glob for dynamic file handling in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import glob from 'fast-glob'; // Import the library
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css', 
-                'resources/js/app.js'
+                'resources/css/app.css',
+                'resources/js/app.js',
+                // Use glob.sync to find all files and expand them into the array
+                ...glob.sync('resources/js/main/**/*.js'),
             ],
             refresh: true,
         }),
